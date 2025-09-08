c4c85cc1f0b0140798087bea9a7e76aa26ff502d
Agoston141 <molnar.agoston2006@gmail.com>
2025-09-08 10:56:18 +0200

    kész alap

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import * as http from "node:http";
+import { env } from "node:process";
+import url from "node:url"
+import path from "node:path"
+import fs from "node:fs/promises"
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+const PORT =process.env.PORT || 3000;
+const LOCALHOST = process.env.LOCALHOST
+
+const server = http.createServer(async(req, res) => {
+    /*res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
-    res.write("Helló, itt a szerver!")
-    res.end("\nGET típusú kérés érkezett a localhost:3000-es portra. Erre érkezett a szerver válasza.");
+    res.write("Ez egy egyszerű http szerver üzenete!")
+    res.end("\nGET típusú kérés érkezett a localhost:3000-es portra. Erre érkezett a szerver válasza.");*/
+
+    const fileName =url.fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(fileName)
+    const indexPath = path.join(__dirname,"index.html")
+    console.log(indexPath);
+    const html = await fs.readFile(indexPath)
+    if(req.url === "/"){
+        res.writeHead(200,{"content-type":"text/html;charset=utf-8"})
+        res.end(html)
+    }else{
+        res.statusCode=404
+        res.end("Page not Found")
+    }
+
 });
 
-server.listen(3000, () => {
-    console.log("The server is running on http://localhost:3000");
+server.listen(PORT, () => {
+    console.log(`The server is running on http://${LOCALHOST}:${PORT}\n`);
 });
